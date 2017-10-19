0acb061588cabb6ea1fd1dc9b0471e5db4bd8083
sevenzzw <Librazzw@163.com>
2017-10-19 17:50:53 +0800

    无限级菜单

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
   selector: 'com-menu',
@@ -6,10 +6,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
+  menu = [
+    {
+      path: 'form',
+      data: {
+        menu: {
+          title: 'form',
+        }
+      },
+      children: [
+        {
+          path: 'inputs',
+          data: {
+            menu: {
+              title: 'inputs',
+            }
+          },
+          children: [
+            {
+              path: 'inputs',
+              data: {
+                menu: {
+                  title: 'inputs',
+                }
+              },
+              children: [
+                {
+                  path: 'inputs',
+                  data: {
+                    menu: {
+                      title: 'inputs',
+                    }
+                  }
+                },
+                {
+                  path: 'inputs',
+                  data: {
+                    menu: {
+                      title: 'inputs',
+                    }
+                  }
+                },
+              ]
+            },
+            {
+              path: 'inputs',
+              data: {
+                menu: {
+                  title: 'inputs',
+                }
+              }
+            },
+          ]
+        },
+        {
+          path: 'inputs',
+          data: {
+            menu: {
+              title: 'inputs',
+            }
+          }
+        },
+      ]
+    }
+  ]
+  isNavOn:boolean = false;
+  @Input() menuItem:any;
+  @Input() child:boolean = false;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.menu);
   }
 
+  navOn(){
+    this.isNavOn?this.isNavOn = false:this.isNavOn = true;
+  }
 }
