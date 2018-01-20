895aee5db1d9405acf61e0d261fbfd64ccb4cb46
sevenzzw <Librazzw@163.com>
2018-01-20 09:48:33 +0800

    1111

--- src/app/shared/animation/collapse-animate.ts
+++ src/app/shared/animation/collapse-animate.ts
@@ -2,8 +2,8 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 
 export const collapse = [
     trigger('collapse', [
-        state('off', style({ 'height': '0', 'padding': '0' })),
-        state('on', style({ 'height': '*' })),
+        state('off', style({ height: 0, padding: 0 })),
+        state('on', style({ height: '*' })),
         transition('* => *', [animate('400ms cubic-bezier(.39,.8,.5,.95)')])
     ])
 ];
