db98d3d2a60bf87c09e2d78684b50622ce746a91
sevenzzw <Librazzw@163.com>
2018-01-29 18:23:24 +0800

    按钮样式 grid样式

--- src/app/shared/services/tables.service.ts
+++ src/app/shared/services/tables.service.ts
@@ -20,7 +20,8 @@ export class TablesService {
         { first_name: 'Steve', last_name: 'Jobs', user_name: '@steve' },
         { first_name: 'Simon', last_name: 'Philips', user_name: '@simon' },
         { first_name: 'Jane', last_name: 'Doe', user_name: '@jane' },
-
+        { first_name: 'Larry', last_name: 'Thornton', user_name: '@larry' },
+        { first_name: 'Hiver', last_name: 'Choe', user_name: '@hiver' },
     ];
     constructor() {
     }
