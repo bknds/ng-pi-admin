185c402caddd5dde59ab615dafac34288bc6aad3
sevenzzw <Librazzw@163.com>
2018-03-12 10:02:17 +0800

    npm install pell editor

--- src/app/pages/ui/components/modals/modals.component.ts
+++ src/app/pages/ui/components/modals/modals.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import pell from 'pell';
+
 
 @Component({
   selector: 'app-modals',
@@ -10,6 +12,21 @@ export class ModalsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    function ensureHTTP(str) {
+      return /^https?:\/\//.test(str) && str || `http://${str}`
+    }
+    
+    pell.init({
+      element: document.getElementById('pell'),
+      defaultParagraphSeparator: 'p',
+      styleWithCSS: true,
+      onChange: function (html) {
+        document.getElementById('text-output').innerHTML = html
+        document.getElementById('html-output').textContent = html
+      }
+    })
+    
+  }
 
 }
